49cd5dcd5c87b063ee2b30cb16edd53b95ad70ff
Anton Keks <anton@codeborne.com>
2011-08-21 20:20:20 +0300

    new photospot can now be added

--- src/ee/devclub/rest/PhotoSpotResource.java
+++ src/ee/devclub/rest/PhotoSpotResource.java
@@ -3,9 +3,7 @@ package ee.devclub.rest;
 import ee.devclub.model.Location;
 import ee.devclub.model.PhotoSpot;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -15,6 +13,12 @@ import static java.util.Arrays.asList;
 public class PhotoSpotResource {
     @GET
     public List<PhotoSpot> getAllSpots() {
-        return asList(new PhotoSpot("Kohtuotsa vaateplatvorm", new Location(59.437755f, 24.74209f)));
+        return asList(new PhotoSpot("Kohtuotsa vaateplatvorm", "", new Location(59.437755f, 24.74209f)));
+    }
+
+    @POST
+    public PhotoSpot newPhotoSpot(@FormParam("name") String name, @FormParam("description") String description,
+                                  @FormParam("lat") float lat, @FormParam("lon") float lon) {
+        return new PhotoSpot(name, description, new Location(lat, lon));
     }
 }
