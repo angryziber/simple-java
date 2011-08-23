2f806185489cb2331d7633bd561b52f06228c1c3
Anton Keks <anton@codeborne.com>
2011-08-23 19:36:37 +0300

    h2 db shell added

---
+++ h2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script will give you H2 database shell
+
+java -cp lib/h2*.jar org.h2.tools.Shell -url 'jdbc:h2:~/.devclub-java/db;AUTO_SERVER=TRUE;USER=devclub;PASSWORD=devclub' -user devclub -password devclub
