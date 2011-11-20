f2700cbd6bacb621bd1775f098d1dad791cdc2d9
Anton Keks <anton@codeborne.com>
2011-11-20 12:47:38 +0200

    service renamed to repository for semantical correctness

--- src/ee/devclub/model/PhotoSpotRepository.java
+++ src/ee/devclub/model/PhotoSpotRepository.java
@@ -1,13 +1,13 @@
 package ee.devclub.model;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
-@Service
-public class PhotoSpotService {
+@Repository
+public class PhotoSpotRepository {
     public List<PhotoSpot> getAllSpots() {
         return asList(new PhotoSpot("Kohtuotsa vaateplatvorm", "", new Location(59.437755f, 24.74209f)));
     }
