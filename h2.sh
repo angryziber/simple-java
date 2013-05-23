148dfd869f3e19ccf381e7b8f52264656c34d1ff
Anton Keks <anton@codeborne.com>
2013-05-23 14:24:36 +0300

    change h2 db location to match the one in master

--- h2.sh
+++ h2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script will give you H2 database shell
 
-java -cp lib/h2*.jar org.h2.tools.Shell -url 'jdbc:h2:~/.devclub-java/db;AUTO_SERVER=TRUE;USER=devclub;PASSWORD=devclub' -user devclub -password devclub
+java -cp lib/default/h2*.jar org.h2.tools.Shell -url 'jdbc:h2:.simple-java;AUTO_SERVER=TRUE' -user sa -password sa
